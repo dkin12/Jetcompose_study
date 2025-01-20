a078fbd147811d99a9fc424b9f2e06955e602404
dkin12 <dkin1020@naver.com>
2025-01-20 21:27:07 +0900

    [이채연] Jetcompose 환경설정

---
+++ app/src/main/java/com/example/jetcompose_study_project/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.jetcompose_study_project
+
+import android.os.Bundle
+import android.provider.MediaStore.Audio.Artists
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetcompose_study_project.ui.theme.Jetcompose_study_projectTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Jetcompose_study_projectTheme {
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text("My App") },
+                            navigationIcon = {
+                                IconButton(onClick = { /* Handle back navigation */ }) {
+                                    Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
+                                }
+                            }
+                        )
+                    },
+                    content = { padding ->
+                        Greeting("Android", Modifier.padding(padding))
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    Column(modifier = modifier.padding(16.dp)) {
+        Text("Hello $name!")
+        Text("Hello Everyone!")
+        Button(onClick = {
+            // Toast 표시
+            Toast.makeText(context, "버튼 클릭", Toast.LENGTH_SHORT).show()
+        }) {
+            Text("Click Me")
+        }
+    }
+}
+
+
+
---
+++ app/src/main/java/com/example/jetcompose_study_project/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.jetcompose_study_project.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFF1A237E)
+val PurpleGrey80 = Color(0xFFCFD8DC)
+val Pink80 = Color(0xFFFF7043)
+
+val Purple40 = Color(0xFF0D1337)
+val PurpleGrey40 = Color(0xFF455A64)
+val Pink40 = Color(0xFFFF5722)
